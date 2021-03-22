6c072d83f2e5f8a22240e3827e83c92436e4c077
Marcilio Cobel do Nascimento Junior <marcilio.nascimento@compasso.com.br>
2021-03-21 23:00:00 -0300

    logando porta

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log('Listening on port %d', port);
+  });
 }
 bootstrap();

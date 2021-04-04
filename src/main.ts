a6e94751c8cf52e13a525a63e130fe09f38cf764
Marcilio Cobel do Nascimento Junior <marcilio.nascimento@compasso.com.br>
2021-04-03 21:46:42 -0300

    Ajuste no cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { GenericExceptionFilter } from './exceptions/generic-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:8080', 'https://cobel-melo.herokuapp.com'],
+      credentials: true,
+    }
+  });
   app.useGlobalFilters(new GenericExceptionFilter());
-  app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     console.log('Listening on port %d', port);
